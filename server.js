58a68c887be7f9fb8c1c0017e17738a9984d3bcf
liutao <1798736436@qq.com>
2017-03-24 13:20:12 +0000

    add mongodb

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var fs = require('fs')
     , http = require('http')
     , socketio = require('socket.io');
+var MongoClient = require('mongodb').MongoClient;//mongodb
+
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
@@ -11,6 +13,20 @@ var server = http.createServer(function(req, res) {
 var userarr=[];
 var userindex;
 var nickname;
+var collection;
+var talkindex=0;
+var DB_CONN_STR = 'mongodb://localhost:27017/ihelpiTalkRoom'; //数据库为 talkDate
+
+//mongod是否连接成功
+MongoClient.connect(DB_CONN_STR, function(err, db) {
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.log("连接成功！");
+        collection = db.collection('talkDate');
+});
+
 
 socketio.listen(server).on('connection',function(socket){        
         socket.on('user join',function(data){
@@ -22,7 +38,6 @@ socketio.listen(server).on('connection',function(socket){
        		socket.broadcast.emit('chat message',{ type:0,msg:'欢迎'+data.user+"进入聊天室",
 			user:data.user });
        	}else{
-       	
        		socket.emit('nickExisted',{msg:1});
 
        	}
@@ -35,13 +50,30 @@ socketio.listen(server).on('connection',function(socket){
 			users:userarr});
 		socket.emit(data.user+'send success',{msg:data.msg,time:time.getTime(),type:data.type});
 
+
+    //插入数据
+    var dataObj = [{"user":data.user,"date":data.msg,"type":data.type,"to":data.to
+                    ,"time":time.getTime()}];
+    collection.insert(dataObj, function(err, result) { 
+        if(err)
+        {
+            console.log('Error:'+ err);
+            return;
+        }    
+       
+    });
+
+
 	});
 	socket.on('disconnect', function() {
     //将断开连接的用户从users中删除
     userarr.splice(userindex, 1);
     //通知除自己以外的所有人
     socket.broadcast.emit('chat message',{type:3,msg:nickname+'离开'});
-});
+  });
 
 })
+
+
+
 
